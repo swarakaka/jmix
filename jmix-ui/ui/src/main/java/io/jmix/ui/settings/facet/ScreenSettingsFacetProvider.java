d964f4777e53103407042c9ba0361dccac43d3f1
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-09-13 16:05:08 +0400

    [ScreenSettingsFacet] ability to disable settings for certain components (812)

--- jmix-ui/ui/src/main/java/io/jmix/ui/settings/facet/ScreenSettingsFacetProvider.java
+++ jmix-ui/ui/src/main/java/io/jmix/ui/settings/facet/ScreenSettingsFacetProvider.java
@@ -51,11 +51,13 @@ public class ScreenSettingsFacetProvider implements FacetProvider<ScreenSettings
 
         loadAuto(element).ifPresent(facet::setAuto);
 
-        List<String> excludedIds = loadOnlyExcludedIds(context, element);
+        Map<String, Boolean> components = loadComponents(context, element);
+
+        List<String> excludedIds = filterExcludedIds(components);
         facet.excludeComponentIds(excludedIds.toArray(new String[0]));
 
         if (!facet.isAuto()) {
-            List<String> ids = loadOnlyIncludedIds(context, element);
+            List<String> ids = filterIncludedIds(components);
             facet.addComponentIds(ids.toArray(new String[0]));
         }
     }
@@ -75,15 +77,15 @@ public class ScreenSettingsFacetProvider implements FacetProvider<ScreenSettings
         return Optional.empty();
     }
 
-    protected List<String> loadOnlyExcludedIds(ComponentLoader.ComponentContext context, Element root) {
-        return loadComponents(context, root).entrySet().stream()
+    protected List<String> filterIncludedIds(Map<String, Boolean> components) {
+        return components.entrySet().stream()
                 .filter(Map.Entry::getValue)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
     }
 
-    protected List<String> loadOnlyIncludedIds(ComponentLoader.ComponentContext context, Element root) {
-        return loadComponents(context, root).entrySet().stream()
+    protected List<String> filterExcludedIds(Map<String, Boolean> components) {
+        return components.entrySet().stream()
                 .filter(entry -> !entry.getValue())
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
@@ -103,8 +105,8 @@ public class ScreenSettingsFacetProvider implements FacetProvider<ScreenSettings
             if (id == null) {
                 throw new GuiDevelopmentException("ScreenSettings component does not define an id", context);
             }
-            String exclude = element.attributeValue("exclude");
-            result.put(id, Boolean.parseBoolean(exclude));
+            String enabled = element.attributeValue("enabled");
+            result.put(id, Strings.isNullOrEmpty(enabled) || Boolean.parseBoolean(enabled));
         }
 
         return result;
